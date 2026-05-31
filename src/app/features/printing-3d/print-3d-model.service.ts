4c148362ead81ae71784c74af737ca7d2c432886
gene-ressler <gene.ressler@gmail.com>
2026-05-31 11:56:11 -0400

    Use uniform taper for 3d printed pier.
    Refactor extrusion parameters for pier in buildPier method.

--- src/app/features/printing-3d/print-3d-model.service.ts
+++ src/app/features/printing-3d/print-3d-model.service.ts
@@ -157,8 +157,9 @@ export class Print3dModelService {
 
   public buildPier(gmy: Print3dGeometry, x: number): Manifold {
     const tab = this.extrudeCentered(gmy.tab, 2 * gmy.tabThickness).translate(gmy.pierXOffset, 0, gmy.pierTopZ);
+    // Don't use different x and y tapers in the extrusion. The diagonal faces become non-planar.
     return this.manifoldClass
-      .extrude(gmy.pier, gmy.pierHeight, undefined, undefined, [gmy.pierTaperX, 1.2])
+      .extrude(gmy.pier, gmy.pierHeight, undefined, undefined, gmy.pierTaperX)
       .rotate(180, 0, 0) // Flip taper.
       .translate(gmy.pierXOffset, 0, gmy.pierTopZ)
       .add(tab)
